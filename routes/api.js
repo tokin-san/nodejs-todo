8ed405c77bec872cc9cfe3f83d5d77cc12ca01c8
tokin-san <51040387+tokin-san@users.noreply.github.com>
2020-05-21 00:47:01 +0900

    CRUD sample

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var user_controller = require('./../lib/controllers/user_controller');
+
 router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+    const users = user_controller.index(req.query);
+    res.json(users);
+});
+
+router.post('/users', function(req, res, next) {
+    res.json(user_controller.store(req.body));
+});
+
+router.put('/users/:id', function(req, res, next) {
+    res.json(user_controller.update(req.params.id, req.body))
+});
+
+router.delete('/users', function(req, res, next) {
+    res.json(user_controller.destroy(req.body))
 });
 
 module.exports = router;
