fdef119cbd5488457e38ad67715a9fbc0e45f75d
inteliser <zxcvisualized4252quick@yahoo.co.jp>
2020-06-16 15:33:26 +0900

    最終変更内容を反映

--- routes/api.js
+++ routes/api.js
@@ -8,16 +8,47 @@ router.get('/users', function(req, res, next) {
     res.json(users);
 });
 
+router.get('/users/:user_id/todos', function(req, res, next) {
+	if(req.query.onlyopen == 1) {
+		const users = user_controller.todo_filter(req.params.user_id, req.query.onlyopen);
+		res.json(users);
+	} else {
+		const users2 = user_controller.todolist(req.params.user_id);
+		res.json(users2);
+	}
+});
+
+router.get('/users/:user_id/todos/:id', function(req, res, next) {
+    const users = user_controller.todo_detail(req.params.id, req.params.user_id);
+    res.json(users);
+});
+
 router.post('/users', function(req, res, next) {
     res.json(user_controller.store(req.body));
+});
+
+router.post('/users/:user_id/todos', function(req, res, next) {
+    res.json(user_controller.storetodo(req.params.user_id, req.body));
 });
 
 router.put('/users/:id', function(req, res, next) {
     res.json(user_controller.update(req.params.id, req.body))
 });
 
+router.put('/users/:user_id/todos/:id', function(req, res, next) {
+    res.json(user_controller.updatetodo(req.params.id, req.params.user_id, req.body))
+});
+
+router.delete('/users/:user_id/todos/:id', function(req, res, next) {
+    res.json(user_controller.destroy_todo(req.params.id, req.params.user_id))
+});
+
+router.delete('/users/:id', function(req, res, next) {
+    res.json(user_controller.destroy(req.params.id));
+});
+
 router.delete('/users', function(req, res, next) {
-    res.json(user_controller.destroy(req.body))
+    res.json(user_controller.destroy_all());
 });
 
 module.exports = router;
